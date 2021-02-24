4f11d3d2ae9c807bba956a572471e502d19643d9
sean.lee <lethee@gmail.com>
2021-02-25 00:43:21 +0900

    initial import

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    console.debug('App.constructor()');
+    console.debug(props);
+    super(props);
+    this.state = {
+      result: null,
+      query: null,
+      status: null,
+    };
+    console.debug(this.state);
+    window.renderedApp = this;
+  }
+
+  render() {
+    console.debug('App.render()');
+    console.debug(this.state);
+    let content = (<div className="Content">Not Loaded</div>);
+    if (this.state.result && this.state.result.issues.length) {
+      content = (<div className="Content">Big</div>);
+    }
+    return (
+      <div className="App">
+        <div>App: {this.state.result ? this.state.result.issues.length: 0}</div>
+        {content}
+        <div className="Status">[{this.state.status}]</div>
+        <div onClick={_ => this.unload()}>(close)</div>
+      </div>
+    );
+  }
+
+  searchJiraIssues() {
+    console.debug('App.searchJiraIssues()');
+    if (this.state.result) {
+      this.setState({status: 'searchJiraIssues already loaded'});
+      return;
+    }
+
+    this.setState({status: 'searchJiraIssues'});
+    setTimeout(_ => {
+      fetch("result.json")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({result: data, status: 'searchJiraIssues done'});
+        this.queryIssueML();
+      })
+      .catch(_ => {
+        this.setState({result: null, status: 'searchJiraIssues error'});
+      })
+    }, 1000);
+  }
+
+  queryIssueML() {
+    console.debug('App.queryIssueML()');
+    this.setState({status: 'queryIssueML'});
+    setTimeout(_ => {
+      fetch("query.json")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({query: data, status: 'queryIssueML done'});
+      })
+      .catch(_ => {
+        this.setState({result: null, status: 'queryIssueML error'});
+      })
+    }, 2000);
+  }
+
+  unload() {
+    console.debug('App.unload()');
+    this.setState({result: null, status: null});
+    window.renderAppUnload();
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './index.css';
+import App from './App';
+
+
+window.renderApp = target => {
+  console.debug("renderApp()");
+  ReactDOM.render(<App env={process.env.REACT_APP_HOST_ENV}/>, target);
+};
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
